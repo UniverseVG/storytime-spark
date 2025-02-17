1c47335b4a2fcdae7c24a904fcd8ca87dd7c5f44
UniverseVG <gmvarun2001@gmail.com>
2025-02-17 23:35:29 +0530

    Refactor user detail handling in NextProvider; improve user loading checks and error handling during database operations

--- app/provider.tsx
+++ app/provider.tsx
@@ -15,38 +15,45 @@ import { UserDetailContext } from "./_context/UserDetailContext";
 import { User } from "@/types";
 
 const NextProvider = ({ children }: { children: React.ReactNode }) => {
-  const [userDetail, setUserDetail] = useState<User>();
-  const { user } = useUser();
+  const [userDetail, setUserDetail] = useState<User | undefined>(undefined);
+
+  const { user, isLoaded } = useUser();
 
   useEffect(() => {
-    if (!user) return;
+    if (!isLoaded || !user) return;
     saveNewUserIfNotExists();
-  }, [user]);
-  const saveNewUserIfNotExists = async () => {
-    const userInfo = await db
-      .select()
-      .from(Users)
-      .where(
-        eq(Users.userEmail, user?.primaryEmailAddress?.emailAddress as string)
-      );
+  }, [isLoaded, user]);
 
-    if (!userInfo?.[0]) {
-      const result: any = await db
-        .insert(Users)
-        .values({
-          userName: user?.fullName,
-          userEmail: user?.primaryEmailAddress?.emailAddress,
-          userImage: user?.imageUrl,
-        })
-        .returning({
-          userEmail: Users?.userEmail,
-          userName: Users?.userName,
-          userImage: Users?.userImage,
-          credits: Users?.credit,
-        });
-      setUserDetail(result);
-    } else {
-      setUserDetail(userInfo?.[0] as User);
+  const saveNewUserIfNotExists = async () => {
+    try {
+      const email = user?.primaryEmailAddress?.emailAddress;
+      if (!email) return;
+
+      const userInfo = await db
+        .select()
+        .from(Users)
+        .where(eq(Users.userEmail, email));
+
+      if (!userInfo?.[0]) {
+        const result: any = await db
+          .insert(Users)
+          .values({
+            userName: user?.fullName,
+            userEmail: email,
+            userImage: user?.imageUrl,
+          })
+          .returning({
+            userEmail: Users.userEmail,
+            userName: Users.userName,
+            userImage: Users.userImage,
+            credit: Users.credit,
+          });
+        setUserDetail(result?.[0]);
+      } else {
+        setUserDetail(userInfo?.[0] as User);
+      }
+    } catch (error) {
+      console.error("Error saving new user", error);
     }
   };
 
