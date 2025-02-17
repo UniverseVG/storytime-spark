6ab549b5f80d9ecaca92179752778a7b7ae028e4
UniverseVG <gmvarun2001@gmail.com>
2025-02-17 19:50:37 +0530

    Simplify middleware by removing route-specific authentication logic and retaining only Clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,10 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/create(.*)"]);
-const isClerkWebhook = createRouteMatcher(["/api/clerk-webhook(.*)"]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isClerkWebhook(req)) {
-    return NextResponse.next();
-  }
-
-  if (isProtectedRoute(req)) {
-    return auth.protect().then(() => NextResponse.next());
-  }
-
-  return NextResponse.next();
-});
-
+export default clerkMiddleware();
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+
     "/(api|trpc)(.*)",
   ],
 };
